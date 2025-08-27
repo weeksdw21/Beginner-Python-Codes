b7773fbf3ecfdd63e179a1391d962c8d30ff2fc6
Donnie Mac <weeksdw21@gmail.com>
2025-08-27 08:02:04 -0400

    Add files via upload

---
+++ Log(2).py
@@ -0,0 +1,7 @@
+import math # Imported math module for logarithm function
+
+x = float(input("Enter number x: ")) # Input for base number
+y = float(input("Enter number y: ")) # Input for exponent
+print(f"{x} raised to the power {y} is {x ** y}") # Exponentiation
+print(f"The log (base 2) of {x} is {math.log2(x)}") # Logarithm base 2
+
